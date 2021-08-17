be8ccc2fe4b2042a438e9b4b962a527fa666dc5f
eewwwwww <tanryan1809@gmail.com>
2021-08-17 15:33:32 +0800

    wqdqw

--- projectAssignment.py
+++ projectAssignment.py
@@ -1,43 +1,86 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot
-import plot as plot
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 
-data = pd.read_excel('dataNew.xls', sheet_name='data')
-print(data)
-dataSplit = data['Period'].str.split(' ', n=1, expand = True)
-print(data.head(11))
-dataSplit[1] = dataSplit[1].astype(str).astype(int)
-print(dataSplit[1])
-print(dataSplit.dtypes)
-df1 = dataSplit[(dataSplit[1] >= 1900) & (dataSplit[1] <= 1910)]
-print(df1[1])
-plt.bar(df1[1], data["Calories"].head(11))
-plt.xticks(df1[1])
-plt.ylabel('Number of Calories')
-plt.xlabel('Year')
-plt.show()
-print(round(data["Calories"].head(11).mean(), 2))
-print(round(data["Calories"].head(11).sum(), 2)) #end of first bar chat
+# dates = pd.read_excel('dataNew.xls')
+# data = dates['Period'].str.split(' ', n=1, expand = True)
+# dates = dates.assign(Year=data[1])
+# dates.index = dates["Year"]
+# print(dates)
+#
+# df1 = dates[(dates["Year"] >= str(1900)) & (dates["Year"] <= str(1910))]
+# qs = df1["Calories"].sort_values();
+# index = np.array(len(qs.index));
+# plt.xlabel('Year')
+# plt.ylabel('Number of Calories')
+# plt.xticks(index, qs.index);
+# plt.title('Data Sheet');
+# plt.bar(qs.index, qs.values);
+# print(plt.show())
+# print(round(data["Calories"].head(11).mean(), 2))
+# print(round(data["Calories"].head(11).sum(), 2)) #end of first bar chat
+#
+# df2 = newdata[(newdata[1] >= str(1911)) & (newdata[1] <= str(1920))]
+# df2[1]["Calories"].sort_values()
+# print(df2[1])
+# plt.bar(df2[1], data[11:21]["Calories"])
+# plt.xticks(df2[1])
+# plt.ylabel('Number of Calories')
+# plt.xlabel('Year')
+# plt.show()
+# print(round(data[11:21]["Calories"].mean(), 2))
+# print(round(data[11:21]["Calories"].sum(), 2)) #end of second bar chat
+#
+# df3 = newdata[(newdata[1] >= 1921) & (newdata[1] <= 1930)]
+# df3[1]["Calories"].sort_values()
+# print(df3[1])
+# plt.bar(df3[1], data[21:31]["Calories"])
+# plt.xticks(df3[1])
+# plt.ylabel('Number of Calories')
+# plt.xlabel('Year')
+# plt.show()
+# print(round(data[21:31]["Calories"].mean(), 2))
+# print(round(data[21:31]["Calories"].sum(), 2)) #end of third bar chat
+datas = pd.read_excel('datanew.xls')
+data = datas['Period'].str.split(' ',n=1, expand=True)
 
-df2 = dataSplit[(dataSplit[1] >= 1911) & (dataSplit[1] <= 1920)]
-print(df2[1])
-plt.bar(df2[1], data[11:21]["Calories"])
-plt.xticks(df2[1])
-plt.ylabel('Number of Calories')
-plt.xlabel('Year')
-plt.show()
-print(round(data[11:21]["Calories"].mean(), 2))
-print(round(data[11:21]["Calories"].sum(), 2)) #end of second bar chat
+datas = datas.assign(Year=data[1])
+datas.index = datas["Year"]
+print(datas)
 
-df3 = dataSplit[(dataSplit[1] >= 1921) & (dataSplit[1] <= 1930)]
-print(df3[1])
-plt.bar(df3[1], data[21:31]["Calories"])
-plt.xticks(df3[1])
-plt.ylabel('Number of Calories')
-plt.xlabel('Year')
-plt.show()
-print(round(data[21:31]["Calories"].mean(), 2))
-print(round(data[21:31]["Calories"].sum(), 2)) #end of third bar chat
+q1 = datas[(datas["Year"] >= str(1900)) & (datas["Year"] <= str(1910))]
+q2 = datas[(datas["Year"] >= str(1911)) & (datas["Year"] <= str(1921))]
+q3 = datas[(datas["Year"] >= str(1921)) & (datas["Year"] <= str(1931))]
 
+ps = q1['Calories'].sort_values()
+index = np.arange(len(ps.index))
+plt.xlabel('Period');
+plt.ylabel('Calories');
+plt.xticks(index, ps.index);
+plt.title('Data Sheet');
+plt.bar(ps.index,ps.values);
+plt.show();
+print(round(datas["Calories"].head(11).mean(), 2))
+print(round(datas["Calories"].head(11).sum(), 2))#end of first bar chat
+
+ps = q2['Calories'].sort_values()
+index = np.arange(len(ps.index))
+plt.xlabel('Period');
+plt.ylabel('Calories');
+plt.xticks(index, ps.index);
+plt.title('Data Sheet');
+plt.bar(ps.index,ps.values);
+plt.show();
+print(round(datas[11:21]["Calories"].mean(), 2))
+print(round(datas[11:21]["Calories"].sum(), 2))
+
+ps = q3['Calories'].sort_values()
+index = np.arange(len(ps.index))
+plt.xlabel('Period');
+plt.ylabel('Calories');
+plt.xticks(index, ps.index);
+plt.title('Data Sheet');
+plt.bar(ps.index,ps.values);
+plt.show();
+print(round(datas[21:31]["Calories"].mean(), 2))
+print(round(datas[21:31]["Calories"].sum(), 2))
