b153d21ab0f13b288870986567d0e5327bd7e0ac
eewwwwww <tanryan1809@gmail.com>
2021-08-19 21:11:05 +0800

    Merge remote-tracking branch 'origin/master'

--- 15_ASP_Project.py
+++ 15_ASP_Project.py
@@ -2,42 +2,45 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-Asia03 = pd.read_excel('IMVA.xls', sheet_name='IMVA')
-print(Asia03)
-print(Asia03.columns)
+Others03 = pd.read_excel('IMVA.xls', sheet_name='IMVA')
+print(Others03)
+print(Others03.columns)
 
-df_AsiaCountries03 = Asia03[['Periods', "Americas", "Canada", "USA", "Other Markets In Americas", "Oceania", "Australia", "New Zealand", "Other Markets In Oceania", "Africa", "Egypt", "Mauritius", "South Africa (Rep Of)", "Other Markets In Africa"]]
-print(df_AsiaCountries03)
+df_OthersCountries03 = Others03[['Periods', "Americas", "Canada", "USA", "Other Markets In Americas", "Oceania", "Australia", "New Zealand", "Other Markets In Oceania", "Africa", "Egypt", "Mauritius", "South Africa (Rep Of)", "Other Markets In Africa"]]
+print(df_OthersCountries03.columns)
+print(df_OthersCountries03)
 
-new = df_AsiaCountries03['Periods'].str.split(' ', n=1, expand=True)
-df_AsiaCountries03 = df_AsiaCountries03.assign(Year=new[0])
+new = df_OthersCountries03['Periods'].str.split(' ', n=1, expand=True)
+df_OthersCountries03 = df_OthersCountries03.assign(Year=new[0])
 
 print('->>>>><<<<<-Splitted Year->>>>><<<<<-')
-df_AsiaCountries03['Year'] = pd.to_numeric(df_AsiaCountries03['Year'])
-pd1 = df_AsiaCountries03['Year'].dtypes
+df_OthersCountries03['Year'] = pd.to_numeric(df_OthersCountries03['Year'])
+pd1 = df_OthersCountries03['Year'].dtypes
 print("Year type is", pd1)
 
-print("->>>>><<<<<-dropped periods->>>>><<<<<-")
-print(df_AsiaCountries03.drop(['Periods'], axis=1))
-df_AsiaCountries33 = df_AsiaCountries03[(df_AsiaCountries03['Year'] >= 2011) & (df_AsiaCountries03['Year'] <= 2020)]
+print("->>>>><<<<<-Dropped Periods->>>>><<<<<-")
+print(df_OthersCountries03.drop(['Periods'], axis=1))
+df_OthersCountries33 = df_OthersCountries03[(df_OthersCountries03['Year'] >= 2011) & (df_OthersCountries03['Year'] <= 2020)]
 
-print('->>>>><<<<<-df_AsiaCountries33->>>>><<<<<-')
-print(df_AsiaCountries33)
+print('->>>>><<<<<-df_OthersCountries33->>>>><<<<<-')
+print(df_OthersCountries33)
+print(df_OthersCountries33.head(3))
+print(df_OthersCountries33.tail(3))
 
-df_AsiaCountries04 = df_AsiaCountries33[["Americas", "Canada", "USA", "Other Markets In Americas", "Oceania", "Australia", "New Zealand", "Other Markets In Oceania", "Africa", "Egypt", "Mauritius", "South Africa (Rep Of)", "Other Markets In Africa"]]
-print('-------df_AsiaCountries04-----')
-print(df_AsiaCountries04)
+df_OthersCountries04 = df_OthersCountries33[["Americas", "Canada", "USA", "Other Markets In Americas", "Oceania", "Australia", "New Zealand", "Other Markets In Oceania", "Africa", "Egypt", "Mauritius", "South Africa (Rep Of)", "Other Markets In Africa"]]
+print('-------df_OthersCountries04-----')
+print(df_OthersCountries04)
 print('sorted')
 
-df_AsiaCountries05 = df_AsiaCountries04.replace(',', '', regex=True)
-df_AsiaCountries06 = df_AsiaCountries05.replace('na', '0', regex=True)
+df_OthersCountries05 = df_OthersCountries04.replace(',', '', regex=True)
+df_OthersCountries06 = df_OthersCountries05.replace('na', '0', regex=True)
 
-print('->>>>><<<<<-df_AsiaCountries06->>>>><<<<<-')
-print(df_AsiaCountries06)
-df_AsiaCountries07 = df_AsiaCountries06.astype(int)
-print(df_AsiaCountries07.dtypes)
-psNotSorted=df_AsiaCountries07.sum()
-psSorted = df_AsiaCountries07.sum().sort_values(ascending=False)
+print('->>>>><<<<<-df_OthersCountries06->>>>><<<<<-')
+print(df_OthersCountries06)
+df_OthersCountries07 = df_OthersCountries06.astype(int)
+print(df_OthersCountries07.dtypes)
+psNotSorted=df_OthersCountries07.sum()
+psSorted = df_OthersCountries07.sum().sort_values(ascending=False)
 
 print('->>>>><<<<<-sorted->>>>><<<<<-')
 print(psSorted)
@@ -67,6 +70,7 @@ plt.show()
 indexAll = np.arange(len(allCountries.index))
 
 plt.figure(figsize=(10, 10))
+plt.title('Top 3 Countries from(Period:2011-2020)')
 plt.xlabel('Countries', fontsize=8)
 plt.ylabel('No. of Travellers (in thousands)', fontsize=8)
 plt.xticks(indexAll, allCountries.index, fontsize=6, rotation=30)
