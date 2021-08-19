df9b85ce3eddd04a29a4ae8577b12cf66f61f3e4
eewwwwww <tanryan1809@gmail.com>
2021-08-19 18:10:37 +0800

    Merge remote-tracking branch 'origin/master'

--- 15_ ASP_project_testcase.py
+++ 15_ ASP_project_testcase.py
@@ -1,16 +1,17 @@
 import unittest
-import marks as prog
+import result as prog
 
+class TestMyProgram(unittest.TestCase):
 
-def test_total(self):
-    data = [45, 60, 70, 40, 80, 90, 55, 75, 20, 65]
-    result = prog.EvaluateMarks.total(data)
-    self.assertEqual(result, 500)
+    def test_total(self):
+        data = [11163284, 9816149, 6549149, 4971870, 1180258, 951180, 680019, 292835, 166877, 123098, 86302, 42084, 14585 ]
+        result = prog.EvaluateMarks.total(data)
+        self.assertEqual(result, 36037690)
 
-def test_mean(self):
-        data = [45, 60, 70, 40, 80, 90, 55, 75, 20, 65]
+    def test_mean(self):
+        data = [11163284, 9816149, 6549149, 4971870, 1180258, 951180, 680019, 292835, 166877, 123098, 86302, 42084, 14585]
         result = prog.EvaluateMarks.mean(data)
-        self.assertEqual(result, 60)
+        self.assertEqual(result, 2772130.0)
 
 if __name__ == '__main__':
     unittest.main()
---
+++ result.py
@@ -0,0 +1,6 @@
+class EvaluateMarks:
+    def total(numlist):
+        return sum(numlist)
+
+    def mean(numlist):
+        return sum(numlist) / len(numlist)
