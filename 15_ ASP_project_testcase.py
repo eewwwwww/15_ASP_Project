2d858f549112085430bf834f2ad999dbf4aa96b3
eewwwwww <tanryan1809@gmail.com>
2021-08-19 21:11:05 +0800

    Merge remote-tracking branch 'origin/master'

--- 15_ ASP_project_testcase.py
+++ 15_ ASP_project_testcase.py
@@ -11,7 +11,7 @@ class TestMyProgram(unittest.TestCase):
     def test_mean(self):
         data = [11163284, 9816149, 6549149, 4971870, 1180258, 951180, 680019, 292835, 166877, 123098, 86302, 42084, 14585]
         result = prog.EvaluateMarks.mean(data)
-        self.assertEqual(result, 2772130.0)
+        self.assertEqual(result, 50)
 
 if __name__ == '__main__':
     unittest.main()
