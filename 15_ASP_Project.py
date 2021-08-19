a377e87fb3af7482a40b3e3e98143a34f7709aca
eewwwwww <tanryan1809@gmail.com>
2021-08-19 13:15:34 +0800

    Merge remote-tracking branch 'origin/master'

--- 15_ASP_Project.py
+++ 15_ASP_Project.py
@@ -3,12 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 dates = pd.read_excel("IMVA.xls", sheet_name="IMVA")
-# print(dates)
-data = dates['Periods'].str.split(' ', n=1, expand = True)
-dates = dates.assign(month=data[1])
-dates.index = dates["month"]
+# data = dates['Periods'].str.split(' ', n=1, expand = True)
+# dates = dates.assign(month=data[1])
+# dates.index = dates["month"]
 print(dates)
 print(dates.columns)
-newdata = ({'Countries': ['Americas', 'Canada', 'USA', 'Other Markets In Americas', 'Oceania', 'Australia', 'New Zealand', 'Other Markets In Oceania', 'Africa', 'Egypt', 'Mauritius', 'South Africa (Rep of)', 'Other Markets In Africa']})
+newdata = {'Countries':['Americas', 'Canada', 'USA', 'Other Markets In Americas', 'Oceania', 'Australia', 'New Zealand', 'Other Markets In Oceania', 'Africa', 'Egypt', 'Mauritius', 'South Africa (Rep of)', 'Other Markets In Africa']}
 df = pd.DataFrame(newdata)
-print()
+df = [['Countries']]
+print(df)
