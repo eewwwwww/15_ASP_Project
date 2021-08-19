4bbd81c7d1b20f6f09027fe85183e73f192b047b
eewwwwww <tanryan1809@gmail.com>
2021-08-19 14:11:29 +0800

    Merge remote-tracking branch 'origin/master'

--- 15_ASP_Project.py
+++ 15_ASP_Project.py
@@ -3,12 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 dates = pd.read_excel("IMVA.xls", sheet_name="IMVA")
-# data = dates['Periods'].str.split(' ', n=1, expand = True)
-# dates = dates.assign(month=data[1])
-# dates.index = dates["month"]
+newdata = dates[["Americas", "Canada", "USA", "Other Markets In Americas", "Oceania", "Australia", "New Zealand", "Other Markets In Oceania", "Africa", "Egypt", "Mauritius", "South Africa (Rep Of)", "Other Markets In Africa"]]
+print(newdata.columns)
+data = dates['Periods'].str.split(' ', n=1, expand = True)
+dates = dates.assign(Year="Periods"[1])
+dates.index = dates["Year"]
 print(dates)
-print(dates.columns)
-newdata = {'Countries':['Americas', 'Canada', 'USA', 'Other Markets In Americas', 'Oceania', 'Australia', 'New Zealand', 'Other Markets In Oceania', 'Africa', 'Egypt', 'Mauritius', 'South Africa (Rep of)', 'Other Markets In Africa']}
+q1 = dates[(dates["Year"] >= str(2011)) & (dates["Year"] <= str(2020))]
 df = pd.DataFrame(newdata)
-df = [['Countries']]
 print(df)
+# print(df.head(3))
+# print(df.tail(3))
